269e47c5141aea6f14117b45f540dfb25fd7526a
antonidlr <antoniodelosrios22@gmail.com>
2022-04-20 19:01:39 -0300

    Function modifications ADD and Cancel button

--- javascript/jstree_test_region.js
+++ javascript/jstree_test_region.js
@@ -49,13 +49,22 @@ addSection[0].addEventListener('click', () => {
             
             if(checkRegion(regionsArray, valueRegion)) {
                 addingRegionHtml(valueRegion);
+                
+                const mainSection = regionSelection();
+                console.log(mainSection);
+                deleteRegion(mainSection);
+                addCountry(mainSection);
+                cancelCountry(mainSection);
+
+                addCity(mainSection);
+                cancelCity(mainSection);
 
                 let region = `${valueRegion}`;
                 let paises = [];
                 let valueReg = {region, paises};
                 regionsArray.push(valueReg);
                 console.log(regionsArray);
-        
+                
                 addInput.style.display = 'none';
                 inputText.value = "";
                 
@@ -91,12 +100,12 @@ function addingRegionHtml (value) {
     <div class="add-item p-hide">
     <input class="add-text" type="text" placeholder="Insertar país">
     <button class="b-add">Guardar</button>
-    <button class="b-add">Cancelar</button>
+    <button class="b-add b-cancel-add">Cancelar</button>
     </div>
     <div class="e-item c-hide">
     <input class="add-text" type="text" placeholder="Editar país">
     <button class="b-add">Actualizar</button>
-    <button class="b-add">Cancelar</button>
+    <button class="b-add b-cancel-edit">Cancelar</button>
     </div>
     </div>
     
@@ -113,12 +122,12 @@ function addingRegionHtml (value) {
     <div class="add-item c-hide">
     <input class="add-text" type="text" placeholder="Insertar ciudad">
     <button class="b-add">Guardar</button>
-    <button class="b-add">Cancelar</button>
+    <button class="b-add b-cancel-add">Cancelar</button>
     </div>
     <div class="e-item c-hide">
     <input class="add-text" type="text" placeholder="Editar ciudad">
     <button class="b-add">Actualizar</button>
-    <button class="b-add">Cancelar</button>
+    <button class="b-add b-cancel-edit">Cancelar</button>
     </div>
     </div>
     </div>
@@ -134,19 +143,13 @@ function checkRegion (data, val) {
             listadoRegion.push(region.region);
         }
     });
- 
+    
     if(listadoRegion.indexOf(val) != -1) {
         return false;
     } else {
         return true;
     }
 }
-
-function deleteRegion () {
-    addEventListener
-}
-
-
 
 
 //Capitalize first letter
@@ -155,6 +158,97 @@ function firsttoUpperCase(str) {
     const str2 = str.charAt(0).toUpperCase() + str.slice(1);
     return str2;
 }
+
+//***************************
+
+function regionSelection () {
+    const parentRegion = document.getElementsByClassName('main-box-region');
+    const pLength = parentRegion.length;
+    return parentRegion[pLength-1];
+    console.log(parentRegion[pLength-1]);
+}
+
+// Función Borrar Region
+
+function deleteRegion(section) {
+    const deleteButton = section.getElementsByClassName('b-delete')[0];
+    deleteButton.addEventListener('click', () => {
+        let result = confirm('¿Desea borrar región?');
+        if(result) {
+            section.remove();
+            const valRegion = section.querySelector('.a-region').innerText;
+            console.log('region '+ valRegion);
+            
+            for(let i=0; i<regionsArray.length; i++) {
+                if(valRegion === regionsArray[i].region) {
+                    regionsArray.splice(i, 1);
+                }
+            }
+        }
+    })
+}
+
+// Función Agregar País
+
+function addCountry(section) {
+    const addButtonCountry = section.getElementsByClassName('add-country')[0];
+    const addItem = section.getElementsByClassName('add-item')[0];
+    const editItem = section.getElementsByClassName('edit-item')[0];
+    addButtonCountry.addEventListener('click', () => {
+        console.log('Add Pais');
+        editItem.style.display = 'none';
+        addItem.style.display = 'flex';
+        addButtonCountry.disabled = true;
+    });
+}
+
+// Función Cancelar agregar país
+
+function cancelCountry(section) {
+    const cancelC = section.getElementsByClassName('b-cancel-add')[0];
+    const addButtonCountry = section.getElementsByClassName('add-country')[0];
+    const addItem = section.getElementsByClassName('add-item')[0];
+    const editItem = section.getElementsByClassName('edit-item')[0];
+    const input = section.querySelectorAll('.add-text')[0];
+    cancelC.addEventListener('click', () => {
+        addItem.style.display = 'none';
+        editItem.style.display = 'flex';
+        addButtonCountry.disabled = false;
+        input.value = "";
+    })
+}
+
+// Función Agregar Ciudad
+
+function addCity(section) {
+    const addButtonCity = section.getElementsByClassName('add-city')[0];
+    const addItem = section.getElementsByClassName('add-item')[1];
+    const editItem = section.getElementsByClassName('edit-item')[1];
+    addButtonCity.addEventListener('click', () => {
+        console.log('Add City');
+        editItem.style.display = 'none';
+        addItem.style.display = 'flex';
+        addButtonCity.disabled = true;
+    })
+}
+
+//Función Cancelar agregar ciudad
+
+function cancelCity(section) {
+    const cancelC = section.getElementsByClassName('b-cancel-add')[1];
+    const addButtonCity = section.getElementsByClassName('add-city')[0];
+    const addItem = section.getElementsByClassName('add-item')[1];
+    const editItem = section.getElementsByClassName('edit-item')[1];
+    const input = section.querySelectorAll('.add-text')[1];
+    cancelC.addEventListener('click', () => {
+        addItem.style.display = 'none';
+        editItem.style.display = 'flex';
+        addButtonCity.disabled = false;
+        input.value = "";
+    })
+}
+
+
 
 
 
